7bca928185c26a69827d2ff6bb21ed9a788eb1bf
aofchanon <chanon.td@gmail.com>
2016-02-23 00:44:46 +0700

    Update Sql & Coding [Page] Gallery

--- wp-content/themes/pht/page-gallery.php
+++ wp-content/themes/pht/page-gallery.php
@@ -1,58 +1,46 @@
 <?php get_header() ?>
     <div class="section-banner"></div>
     <div class="container">
-    <h1 class="head-web">GALLERY</h1>
+        <h1 class="head-web">GALLERY</h1>
 
-    <div class="gallery-menu">
+        <div class="gallery-menu">
+            <?php
+
+            $args = array('hide_empty' => 0);
+            $terms = get_terms('package-category', $args);
+            if (!empty($terms) && !is_wp_error($terms)) {
+                echo '<ul class="list-unstyled">';
+                echo '<li><a href="javascript:;" data-filter="*">Show all</a></li>';
+                foreach ($terms as $term) {
+                    echo '<li><a href="javascript:;" data-filter=".ids' . $term->term_id . '">' . $term->name . '</a></li>';
+                }
+                echo '</ul>';
+            }
+            ?>
+        </div>
+        <hr>
+
+
         <?php
 
-        $args = array('hide_empty' => 0);
-        $terms = get_terms('package-category', $args);
-        if (!empty($terms) && !is_wp_error($terms)) {
-            echo '<ul class="list-unstyled">';
-            echo '<li><a href="javascript:;" data-filter="*">Show all</a></li>';
-            foreach ($terms as $term) {
-                echo '<li><a href="javascript:;" data-filter=".ids' . $term->term_id . '">' . $term->name . '</a></li>';
+        $rows = get_field('gallery_pic');
+        if ($rows) {
+            echo '<div class="row posts">';
+
+            foreach ($rows as $row) {
+                //echo '<li>sub_field_1 = ' . $row['sub_field_1'] . ', sub_field_2 = ' . $row['sub_field_2'] . ', etc</li>';
+                $image = $row['gallery_pic_'];
+                $term = $row['packages_gallery'];
+
+                echo '<div class="item col-sm-3 ids' . $term[0] . '" >
+                <a href = "' . $image['url'] . '" ><img class="img-responsive " src = "' . $image['url'] . '" ></a >
+            </div >';
             }
-            echo '</ul>';
+            echo '</div>';
         }
         ?>
+
     </div>
-    <hr>
-    <div class="row posts">
 
-<?php
 
-$rows = get_field('gallery_pic');
-if ($rows) {
-    echo '<ul>';
-
-    foreach ($rows as $row) {
-        echo '<li>sub_field_1 = ' . $row['sub_field_1'] . ', sub_field_2 = ' . $row['sub_field_2'] . ', etc</li>';
-    }
-
-    echo ' <div class="item col-sm-3 ids5" >
-                <a href = "//lorempixel.com/600/800/business/" ><img class="img-responsive " src = "//lorempixel.com/600/800/business/" ></a >
-            </div >';
-}
-?>
-
-     <!--      <div class="item col-sm-3 ids5" >
-                <a href = "//lorempixel.com/600/800/business/" ><img class="img-responsive " src = "//lorempixel.com/600/800/business/" ></a >
-            </div >
-
-            <div class="item col-sm-3" >
-                <a href = "//lorempixel.com/800/600/city" ><img class="img-responsive" src = "//lorempixel.com/800/600/city" ></a >
-            </div >
-            <div class="item col-sm-3" >
-                <a href = "//lorempixel.com/600/800/business/" ><img class="img-responsive" src = "//lorempixel.com/600/800/business/" ></a >
-            </div >
-
-            <div class="item col-sm-3" >
-                <a href = "//lorempixel.com/800/600/city" ><img class="img-responsive" src = "//lorempixel.com/800/600/city" ></a >
-            </div >
-       -->
-        </div >
-    </div >
-
-<?php get_footer()?>
+<?php get_footer() ?>
